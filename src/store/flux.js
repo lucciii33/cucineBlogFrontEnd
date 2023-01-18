4b4741e5617175b3c6267de774d421db547abd11
lucciii33 <angelomaiele@gmail.com>
2023-01-18 16:46:58 +0000

    more

--- src/store/flux.js
+++ src/store/flux.js
@@ -7,10 +7,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 		actions: {
 			loginToken: async (email, password) => {
 				console.log("function called")
-				const resp = await fetch('http://localhost:5000/api/user/login', {
+				const resp = await fetch('https://5000-lucciii33-blogcucinebac-1rglp335x37.ws-us83.gitpod.io/api/users/login', {
 				  method: "POST",
 				  headers: { "Content-Type": "application/json",
-				"Access-Control-Allow-Origin":"*"
+				// "Access-Control-Allow-Origin": '*'
 				},
 				  body: JSON.stringify({ email, password })
 				});
@@ -29,8 +29,24 @@ const getState = ({ getStore, getActions, setStore }) => {
 				// sessionStorage.setItem("jwt-token", data.access_token);
 				// console.log(loggId)
 				return data
-		
 			  },
+
+			  registerUser: async (email, password) => {
+				const resp = await fetch(`https://5000-lucciii33-blogcucinebac-1rglp335x37.ws-us83.gitpod.io/api/users`, {
+				  method: "POST",
+				  headers: { "Content-Type": "application/json" },
+				  body: JSON.stringify({ email, password })
+				});
+		
+				if (!resp.ok) throw "Problem with the response";
+		
+				if (resp.status === 401) {
+				  throw "Invalid credentials";
+				} else if (resp.status === 400) {
+				  throw "Invalid email or password format";
+				}
+			  },
+
 			  
 	}};
 };
